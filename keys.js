a833bcd624c971414965f3615dd2ccbc9fa9bf52
CodeQuiver <38484031+CodeQuiver@users.noreply.github.com>
2018-06-07 23:08:03 -0400

    twitter basic pull successfully set up

--- keys.js
+++ keys.js
@@ -5,8 +5,9 @@ exports.twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-};
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET};
+//   my_user_id: USERID
+
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
--- liri.js
+++ liri.js
@@ -1,10 +1,31 @@
+// configuration
 require("dotenv").config();
+
+
+// Take in the command line arguments
+var nodeArgs = process.argv;
+
+// will need fs to read and write files
+var fs = require("fs");
+
 
 var request = require('request');
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 
-var keys = require("keys.js");
+var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+// Building the twitter API call via the convenience code setup by the twitter node module
+client.get('statuses/user_timeline', {count: 2}, function(error, tweets, response) {
+    if(error) throw error;
+
+    //Need a for loop here to loop through each tweet in array
+    console.log("Tweet " + (0 + 1)+ ": " + tweets[0].text);
+    console.log("Posted: " + tweets[0].created_at + "\n");
+
+    // console.log(JSON.stringify(response));  // Raw response object. stringified
+  });
+
